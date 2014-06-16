6d6abc0836e0df9c14be7688d2658d43fe35af15
Nitya Oberoi <nitya@quirky.com>
2014-06-16 18:31:51 -0400

    Stupid error

--- app.py
+++ app.py
@@ -82,5 +82,5 @@ def process_hook():
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
+    port = int(environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
